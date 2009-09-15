da1105746fbc804a3de60a37498704198c37cc3e
Olivier Gillet <olivier@Radha.local>
2009-09-15 03:16:38 +0200

    - Revised comments on envelopes code.

--- hardware/shruti/envelope.h
+++ hardware/shruti/envelope.h
@@ -44,6 +44,13 @@ class Envelope {
   void Render() {
     uint16_t value = value_;
     value_ += increment_;
+    // This code makes the assumption that only the ATTACK stage has a positive
+    // slope. This is true for the classical ADSR envelope. To support more
+    // complex multistage envelopes, the correct code is:
+    //
+    // if ((increment_ > 0) ^ (value_ < target_)) {
+    //
+    // but the first test is more expensive on AVR...
     if ((stage_ == ATTACK) ^ (value_ < target_)) {
       value_ = target_;
       ++stage_;
@@ -52,11 +59,12 @@ class Envelope {
   }
 
  private:
-  uint8_t release_;
-  uint8_t stage_;
-  int16_t increment_;
-  int16_t target_;
-  int16_t value_;
+  uint8_t release_;  // release time.
+  uint8_t stage_;  // current envelope stage.
+  int16_t increment_;  // envelope value increment.
+  int16_t target_;  // target value (moves to next stage once reached).
+  int16_t value_;  // envelope value, 0-16384.
+  // Increment and target for each stage of the envelope.
   int16_t stage_increment_[UNKNOWN + 1];
   int16_t stage_target_[UNKNOWN + 1];
    
