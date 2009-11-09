76b13a77a522f6240206c4f8480ca8f35577db86
Olivier Gillet <olivier@Radha.local>
2009-11-09 01:32:58 +0100

    Added GPL notice to all source files, cc-by-nc-sa to all PCB design files

--- hardware/shruti/resources/resources.py
+++ hardware/shruti/resources/resources.py
@@ -1,12 +1,40 @@
 #!/usr/bin/python2.5
 #
-# Copyright 2009 Olivier Gillet (ol.gillet@gmail.com). All rights reserved
+# Copyright 2009 Olivier Gillet.
+#
+# Author: Olivier Gillet (ol.gillet@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------------
 #
 # Master resources file.
 
-header = """// Copyright 2009 Olivier Gillet. All rights reserved
+header = """// Copyright 2009 Olivier Gillet.
 //
 // Author: Olivier Gillet (ol.gillet@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// -----------------------------------------------------------------------------
 //
 // Resources definitions.
 //
--- hardware/utils/logging.h
+++ hardware/utils/logging.h
@@ -1,9 +1,24 @@
-// Copyright 2009 Olivier Gillet. All rights reserved
+// Copyright 2009 Olivier Gillet.
 //
 // Author: Olivier Gillet (ol.gillet@gmail.com)
 //
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// -----------------------------------------------------------------------------
+//
 // Googley logging/assert functions. Used for the unit tests and for making
 // development/debugging on a non-AVR platform (eg OS X) easier.
+// (inspired by logging/assert functions from several Google open source,
+// projects, eg plda).
 
 #ifndef HARDWARE_UTILS_LOGGING_H_
 #define HARDWARE_UTILS_LOGGING_H_
